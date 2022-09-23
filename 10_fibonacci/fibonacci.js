a372c54c6187f4c47f522af1522ca0ce98f64087
H Brooke <hbrooke36@yahoo.com>
2022-09-23 13:25:23 -0700

    Solve Exercise 10 - Fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    Number(index);
+    if (index < 0){
+        return "OOPS";
+    }
+    if (index == 0 ){
+        return 0;
+    }
+    if (index == 1){
+        return 1;
+    }
+    else {
+        return fibonacci(index-1) + fibonacci(index-2)
+    }
 
 };
 
