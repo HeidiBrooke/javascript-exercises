333807fa503b93e05943a4239b12181ebeb7a1e2
H Brooke <hbrooke36@yahoo.com>
2022-09-23 12:26:10 -0700

    Solve Exercise 8 - calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,49 @@
-const add = function() {
+const add = function(a, b) {
+  let addResult = a + b;
+  return addResult;
 	
 };
 
-const subtract = function() {
+const subtract = function(a, b) {
+  let subtractResult = a - b;
+  return subtractResult;
 	
 };
 
-const sum = function() {
+const sum = function(arr) {
+  let sum = 0;
+  for (i = 0; i < arr.length; i++){
+    sum += arr[i];
+  }
+  return sum;
 	
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  let multiplyResult = 1;
+  for (i = 0; i < arr.length; i++){
+    multiplyResult *= arr[i];
+  }
+  return multiplyResult;
 
 };
 
-const power = function() {
+const power = function(a, b) {
+  let powerResult = a ** b;
+  return powerResult;
 	
 };
 
-const factorial = function() {
+const factorial = function(num) {
+  if ( num < 1 ) {
+    return 1;
+  }
+  else if (num == 1){
+    return 1;
+  }
+  else {
+    return num * (factorial(num-1));
+  }
 	
 };
 
