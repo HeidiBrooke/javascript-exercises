d946067ad4e076b311473e1f572662a8fe376011
HeidiBrooke <hbrooke36@yahoo.com>
2022-10-12 10:27:23 -0700

    Solve exercise findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function(personsArray) {
+    const oldest = personsArray.sort(function(a, b){
+        const lastPerson = Number(stillLiving(a)) - a.yearOfBirth;
+        const nextPerson = Number(stillLiving(b)) - b.yearOfBirth;
+        return lastPerson > nextPerson ? -1 : 1;
+     })
+    console.table(oldest);
+    return oldest[0];
+}
 
-};
+
+function stillLiving(person){
+    const currentYear = new Date().getFullYear();
+    if ('yearOfDeath' in person){
+      return person.yearOfDeath; 
+    }
+    return currentYear;
+}
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
